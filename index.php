80d64ae15d20efe03d176ee4e7e5574c9b7ffdfb
Thomas Json <joogletom8@gmail.com>
2022-10-20 16:10:11 +0200

    Autoloader.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
- echo 'hello Boutique';
-?>
+
+require_once './Autoloader.php';
+
+Autoloader::register();
+
+$request = $_SERVER['REQUEST_METHOD'] . "/" .
+    filter_var(trim($_SERVER["REQUEST_URI"], '/'), FILTER_SANITIZE_URL);
+
+    echo $request;
+
+$controller = new Controllers\DatabaseController($request);
