54baa450025953a005b8ff64c2aaf7e016ba8994
JustineDev00 <dev.justine.verin@gmail.com>
2022-10-21 17:38:19 +0200

    sprint 1

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $_ENV['env'] = $env;
 
 require_once 'autoload.php';
 
+<<<<<<< HEAD
 use helpers\HttpRequest;
 use helpers\HttpResponse;
 use services\DatabaseService;
@@ -24,3 +25,19 @@ $controller = new DatabaseController($request);
 // HttpResponse::send(["data"=>$result]);
 
 HttpResponse::send(["message" => "La table " . $request->route[0] . " existe."]);
+=======
+use Helpers\HttpRequest;
+use Helpers\HttpResponse;
+use Services\DatabaseService;
+use Controllers\DatabaseController;
+
+$request = HttpRequest::instance();
+$tables = DatabaseService::getTables();
+if(empty($request->route) || !in_array($request->route[0], $tables)){
+    HttpResponse::exit();
+}
+$controller = new DatabaseController($request);
+//$result = $controller->execute();
+//HttpResponse::send(["data"=>$result]);
+HttpResponse::send(["message"=>"La table ".$request->route[0]." existe."]);
+>>>>>>> fe6e448 (sprint 1)
